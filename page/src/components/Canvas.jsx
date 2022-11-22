42295b9a8df1849ec55d8214326c87368e85db31
Simon Stijnen <simon.stijnen@student.howest.be>
2022-11-22 14:03:39 +0100

    Moving dots

--- page/src/components/Canvas.jsx
+++ page/src/components/Canvas.jsx
@@ -2,44 +2,35 @@ import React from 'react';
 import { useRef, useState, useEffect, useMemo } from 'react';
 import config from '../config.json';
 
+import icon_move from '../images/icons/canvas/move.svg';
+
 const padding = 10;
 const scale = 0.5;
 
 const svgHeight = 400 + padding * 2;
-const svgWidth = 800 + padding * 2;
+const svgWidth = 880 + padding * 2;
 
 const dot_radius = 6;
 
-const sample_dots = [
-    {
-        name: 'Keuken1',
-        addr: '03:21:32:0d',
-        type: 'Sateliet',
-        x: 100,
-        y: 200,
-    },
-    {
-        name: 'Keuken2',
-        addr: '03:21:32:0d',
-        type: 'Satelietje',
-        x: 750,
-        y: 50,
-    },
-    {
-        name: 'Living',
-        addr: '03:21:32:0d',
-        type: 'uwu',
-        x: 100,
-        y: 100,
-    },
-    {
-        name: 'TV',
-        addr: '03:21:32:0d',
-        type: 'Maxim',
-        x: 550,
-        y: 350,
-    },
-];
+const moveDotDefault = [null, null];
+let moveDot = moveDotDefault;
+
+// const sample_dots = [
+//     {
+//         name: 'Keuken1',
+//         addr: '03:21:32:0d',
+//         type: 'Sateliet',
+//         x: 780,
+//         y: 20,
+//     },
+//     {
+//         name: 'Keuken2',
+//         addr: '03:21:32:0d',
+//         type: 'Satelietje',
+//         x: 780,
+//         y: 370,
+//     },
+// ];
 
 const emptyDotsInfo = {
     focusedDot: {
@@ -56,17 +47,22 @@ const emptyDotsInfo = {
         addr: '',
     },
     type: 'None',
+    selectedDot: null,
 };
 
-const Canvas = ({ height, width, satelites, devices }) => {
+const Canvas = ({ collection, websocket }) => {
     const Dot = useMemo(() => {
         return class Dot {
             constructor(x, y) {
                 this.x = x;
                 this.y = y;
+                this.moveble = false;
             }
 
             getDotSVG(i) {
+                const x = this.x ? parseInt(this.x) + padding : 0 + padding;
+                const y = this.y ? parseInt(this.y) + padding : 0 + padding;
+
                 return (
                     <g
                         key={i}
@@ -94,22 +90,19 @@ const Canvas = ({ height, width, satelites, devices }) => {
                                     y: this.y,
                                 },
                                 type: this.type,
+                                selectedDot: this,
                             });
                         }}
                     >
                         <circle
-                            cx={parseInt(this.x) + padding}
-                            cy={parseInt(this.y) + padding}
+                            cx={x}
+                            cy={y}
                             r={dot_radius}
                             stroke={'#000'}
                             strokeWidth={2}
                             fill={'#0000ff75'}
                         />
-                        <circle
-                            cx={parseInt(this.x) + padding}
-                            cy={parseInt(this.y) + padding}
-                            r={dot_radius / 2}
-                        />
+                        <circle cx={x} cy={y} r={dot_radius / 2} />
                     </g>
                 );
             }
@@ -126,55 +119,77 @@ const Canvas = ({ height, width, satelites, devices }) => {
                 this.addr = addr;
                 this.type = 'Satelite';
                 this.devices = [];
+                this.moveble = true;
+            }
+            addDevice(dev) {
+                this.devices.push(dev);
+            }
+            setPosition(x, y) {
+                this.x = x;
+                this.y = y;
             }
         };
     }, [Dot]);
 
     const Device = useMemo(() => {
         return class Device extends Dot {
-            constructor(x, y, name, mac, rssi) {
-                super(x, y, name, mac, rssi);
-                this.x = x;
-                this.y = y;
+            constructor(name, mac, rssi) {
+                super(name, mac, rssi);
+                this.x = null;
+                this.y = null;
                 this.name = name;
                 this.mac = mac;
                 this.rssi = rssi;
-            }
-            
-            radius(i, signal_strength) {
-                return (
-                    <g key={i}>
-                        <circle
-                            cx={parseInt(this.x) + padding}
-                            cy={parseInt(this.y) + padding}
-                            r={signal_strength * scale}
-                            stroke={'#000'}
-                            strokeWidth={2}
-                        />
-                    </g>
-                );
+                this.moveble = false;
             }
         };
     }, [Dot]);
 
+    const updatePosition = (mac, x, y) => {
+        websocket.send(`UPDATE_POSITION=${mac}&${x}&${y}`);
+    };
+
     const svgRef = useRef(null);
     const canvasRef = useRef(null);
+    const dotInfoRef = useRef(null);
     const [dots, setDots] = useState([]);
+    // const [sateliteDots, setSateliteDots] = useState([]);
     const [dotInfo, setDotInfo] = useState(emptyDotsInfo);
 
     useEffect(() => {
         setDots(() => {
             const sats = [];
-            for (const sat of satelites) {
-                sats.push(new Satelite(sat.x, sat.y, sat.name, sat.mac));
+            for (const col of collection) {
+                const s = new Satelite(
+                    col.satelite.x,
+                    col.satelite.y,
+                    col.satelite.name,
+                    col.satelite.mac
+                );
+                for (const dev of col.devices) {
+                    s.addDevice(new Device(dev.name, dev.addr, dev.rssi));
+                }
+                sats.push(s);
             }
             return [...sats];
         });
-    }, [satelites, devices, Satelite, Device]);
+    }, [collection, Satelite, Device]);
 
-    const DotInfo = ({ x, y, visibible, type, dot, focusedDot }) => {
+    const DotInfo = ({ x, y, visibible, type, dot, selectedDot }) => {
+        const Button = ({ img, text, onClick }) => {
+            return (
+                <>
+                    <div className="dotInfo-button" onClick={onClick}>
+                        <img src={img} alt={`${text} Icon`} />
+                        <div>{text}</div>
+                    </div>
+                </>
+            );
+        };
+
         return (
             <div
+                ref={dotInfoRef}
                 className="dotInfo"
                 style={{
                     left: x,
@@ -188,8 +203,18 @@ const Canvas = ({ height, width, satelites, devices }) => {
                     <div>{dot.addr}</div>
                 </div>
                 <div>
-                    Position: {dot.x ? dot.x : 0}, {dot.y ? dot.y : 0}
+                    Position: {dot.x ? Math.floor(dot.x) : '?'},{' '}
+                    {dot.y ? Math.floor(dot.y) : '?'}
                 </div>
+                {selectedDot?.moveble ? (
+                    <Button
+                        img={icon_move}
+                        text={'Move'}
+                        onClick={(event) => MoveDot(event, selectedDot)}
+                    />
+                ) : (
+                    <></>
+                )}
             </div>
         );
     };
@@ -213,52 +238,36 @@ const Canvas = ({ height, width, satelites, devices }) => {
 
     const HandleClick = (event) => {
         if (event.target.parentElement.localName === 'g') return;
+        const canvas = event.target.parentElement.parentElement;
+        const container =
+            event.target.parentElement.parentElement.parentElement
+                .parentElement;
+        const svg = svgRef.current;
+        const eve_clientX = event.clientX;
+        const eve_clientY = event.clientY;
+        const eve_offsetLeft = svg.parentElement.offsetLeft;
+        const eve_offsetTop = svg.parentElement.offsetTop;
+        const svg_width = svg.clientWidth;
+        const svg_height = svg.clientHeight;
+        const par_width = event.target.parentElement.clientWidth;
+        const par_height = event.target.parentElement.clientHeight;
 
-        // const svg = svgRef.current;
-        // const eve_clientX = event.clientX;
-        // const eve_clientY = event.clientY;
-        // const eve_offsetLeft = svg.parentElement.offsetLeft;
-        // const eve_offsetTop = svg.parentElement.offsetTop;
-        // const svg_width = svg.clientWidth;
-        // const svg_height = svg.clientHeight;
-        // const par_width = event.target.parentElement.clientWidth;
-        // const par_height = event.target.parentElement.clientHeight;
-        // if (
-        //     !eve_clientX ||
-        //     !eve_clientY ||
-        //     !eve_offsetLeft ||
-        //     !eve_offsetTop ||
-        //     !svg_width ||
-        //     !svg_height ||
-        //     !par_width ||
-        //     !par_height
-        // ) {
-        //     return;
-        // }
+        if (moveDot[0] !== null) {
+            const [, dot] = moveDot;
 
-        // const container =
-        //     event.target.parentElement.parentElement.parentElement
-        //         .parentElement;
-        // const canvas = event.target.parentElement.parentElement;
-
-        // setDots(() => {
-        //     return [
-        //         ...dots,
-        //         {
-        //             x:
-        //                 ((eve_clientX - eve_offsetLeft) * svg_width) /
-        //                     par_width -
-        //                 dot_radius * 2.4 +
-        //                 canvas.scrollLeft,
-        //             y:
-        //                 ((eve_clientY - eve_offsetTop) * svg_height) /
-        //                     par_height -
-        //                 dot_radius * 2.4 +
-        //                 canvas.scrollTop +
-        //                 container.scrollTop,
-        //         },
-        //     ];
-        // });
+            dot.setPosition(
+                Math.floor(((eve_clientX - eve_offsetLeft) * svg_width) / par_width +
+                    canvas.scrollLeft +
+                    container.scrollLeft -
+                    dot_radius * 1.6),
+                Math.floor(((eve_clientY - eve_offsetTop) * svg_height) / par_height +
+                    canvas.scrollTop +
+                    container.scrollTop -
+                    dot_radius * 1.6)
+            );
+            updatePosition(dot.addr, dot.x, dot.y);
+            moveDot = moveDotDefault;
+        }
 
         setDotInfo(emptyDotsInfo);
     };
@@ -268,92 +277,60 @@ const Canvas = ({ height, width, satelites, devices }) => {
         const scrollLeft = event.currentTarget.scrollLeft;
 
         setDotInfo((prev) => {
-            return {
-                focusedDot: {
-                    x: prev.focusedDot.x,
-                    y: prev.focusedDot.y,
-                },
-                visibible: prev.visibible,
-                x: prev.focusedDot.x - scrollLeft,
-                y: prev.focusedDot.y - scrollTop,
-                dot: {
-                    x: prev.dot.x,
-                    y: prev.dot.y,
-                    name: prev.dot.name,
-                    addr: prev.dot.addr,
-                },
-                type: prev.type,
-            };
+            prev.x = prev.focusedDot.x - scrollLeft;
+            prev.y = prev.focusedDot.y - scrollTop;
+            return prev;
         });
     };
 
-    return (
-        <div
-            ref={canvasRef}
-            className="canvas"
-            onScroll={HandleScroll}
-            onClick={HandleClick}
-        >
-            <svg ref={svgRef} height={svgHeight} width={svgWidth}>
-                {drawRoom(config.rooms.Living.corners)}
-                {dots.map((dot, i) => {
-                    return dot.getDotSVG(i);
-                })}
-                {dots.map((dot, i) => {
-                    console.log(dots);
-                    if (dot.addr !== '00:04:4b:84:44:74') return <></>;
-                    return dot.radius(i, 50);
-                })}
-                {sample_dots.map((dot, i) => {
-                    return (
-                        <g
-                            key={i}
-                            onClick={(event) => {
-                                const eve_clientX = event.clientX;
-                                const eve_clientY = event.clientY;
-                                const canvas =
-                                    event.target.parentElement.parentElement
-                                        .parentElement;
-                                const scrollTop = canvas.scrollTop;
-                                const scrollLeft = canvas.scrollLeft;
+    const MoveDot = (event, dot) => {
+        console.log('Select where the satelite is located.');
+        // setDotInfo((prev) => {
+        //     return emptyDotsInfo;
+        // });
 
-                                setDotInfo({
-                                    focusedDot: {
-                                        x: eve_clientX + scrollLeft,
-                                        y: eve_clientY + scrollTop,
-                                    },
-                                    visibible: true,
-                                    x: eve_clientX,
-                                    y: eve_clientY,
-                                    dot: {
-                                        name: dot.name ? dot.name : 'name',
-                                        addr: dot.addr ? dot.addr : 'addr',
-                                        x: dot.x,
-                                        y: dot.y,
-                                    },
-                                    type: dot.type ? dot.type : 'type',
-                                });
-                            }}
-                        >
-                            <circle
-                                cx={parseInt(dot.x) + padding}
-                                cy={parseInt(dot.y) + padding}
-                                r={dot_radius}
-                                stroke={'#000'}
-                                strokeWidth={2}
-                                fill={'#0000ff75'}
-                            />
-                            <circle
-                                cx={parseInt(dot.x) + padding}
-                                cy={parseInt(dot.y) + padding}
-                                r={dot_radius / 2}
-                            />
-                        </g>
-                    );
-                })}
-            </svg>
+        moveDot = [event, dot];
+        // Rest gets handled in HandleClick
+    };
+
+    return (
+        <>
+            <div
+                ref={canvasRef}
+                className="canvas"
+                onScroll={HandleScroll}
+                onClick={HandleClick}
+            >
+                <svg ref={svgRef} height={svgHeight} width={svgWidth}>
+                    {drawRoom(config.rooms.Living.corners)}
+                    {/* {dots.map((dot, i) => {
+                        for (const dev of dot.devices) {
+                            if (dev.mac !== '00:04:4b:84:44:74') break;
+
+                            return (
+                                <>
+                                    <g key={i + 0.1}>
+                                        <circle
+                                            cx={parseInt(dot.x) + padding}
+                                            cy={parseInt(dot.y) + padding}
+                                            r={Math.abs(dev.rssi)}
+                                            stroke={'#000'}
+                                            strokeWidth={2}
+                                            fill={'transparent'}
+                                        />
+                                    </g>
+                                </>
+                            );
+                        }
+                        return <></>;
+                    })} */}
+                    {dots.map((dot, i) => {
+                        return dot.getDotSVG(i);
+                    })}
+                </svg>
+            </div>
             <DotInfo {...dotInfo} />
-        </div>
+        </>
     );
 };
 
--- page/src/pages/Contact.jsx
+++ page/src/pages/Contact.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import Canvas from '../components/Canvas';
 
-const Contact = ({ satelites, devices }) => {
+const Contact = ({ collection, websocket }) => {
     return (
         <>
             <h1>Canvas</h1>
-            <Canvas satelites={satelites} devices={devices} />
+            <Canvas collection={collection} websocket={websocket} />
         </>
     );
 };
