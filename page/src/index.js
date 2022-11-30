95b698dcd529578c43086398b8cbe88f3c9f509c
Simon Stijnen <simon.stijnen@student.howest.be>
2022-11-30 10:05:36 +0100

    Formatted index.js

--- page/src/index.js
+++ page/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import './App.css';
-import './pages/Home.css'
+import './pages/Home.css';
 import './components/Sidebar.css';
 import './components/Satelite.css';
 import './components/Device.css';
@@ -13,9 +13,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-   <React.StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-   </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+                <App />
+        </BrowserRouter>
+    </React.StrictMode>
 );
