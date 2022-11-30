564994dceaa9c51e63d6f742f872a5b3dd6c5ba2
Simon Stijnen <simon.stijnen@student.howest.be>
2022-11-30 10:04:08 +0100

    Add theme option to config

--- page/src/pages/Settings.jsx
+++ page/src/pages/Settings.jsx
@@ -1,11 +1,23 @@
-import React from "react";
+import config from '../config.json'
 
-const Settings = () => {
+const Settings = ({ theme , setTheme}) => {
+    const toggleTheme = () => {
+        switch (theme) {
+            case config.themes.light:
+                return setTheme(config.themes.dark);
+            case config.themes.dark:
+                return setTheme(config.themes.light);
+            default:
+                return setTheme(config.themes.light);
+        }
+    };
+
     return (
         <>
             <h1>Settings</h1>
+            <button onClick={toggleTheme}>Toggle Theme: {theme}</button>
         </>
-    )
-}
+    );
+};
 
-export default Settings
+export default Settings;
