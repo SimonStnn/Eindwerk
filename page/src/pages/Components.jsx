18df19686dada953cfb7b63ecf7a17dd5de41312
Simon Stijnen <simon.stijnen@student.howest.be>
2022-12-30 12:53:40 +0100

    Add minor text to components page

--- page/src/pages/Components.jsx
+++ page/src/pages/Components.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const Components = () => {
     return (
-        <>
+        <div className="components">
             <h1>Components</h1>
             <div>
-
+                This is used for my gip
             </div>
-        </>
+        </div>
     );
 }
 
