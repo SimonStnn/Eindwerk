612ce5e0b5c18fb4dcb96f4476ec745b222729b8
Simon Stijnen <simon.stijnen@student.howest.be>
2023-02-03 14:10:44 +0100

    Put Sidebar in React.memo

--- page/src/components/Sidebar.jsx
+++ page/src/components/Sidebar.jsx
@@ -21,71 +21,86 @@ const link_discover = '/discover';
 const link_components = '/components';
 const link_settings = '/settings';
 
-const Sidebar = () => {
-    const [sidebarOpen, setSideBarOpen] = useState(false);
-    const toggleSidebar = () => {
-        setSideBarOpen(!sidebarOpen);
-    };
+const Sidebar = React.memo(
+    () => {
+        const [sidebarOpen, setSideBarOpen] = useState(false);
+        const toggleSidebar = () => {
+            setSideBarOpen(!sidebarOpen);
+        };
 
-    const SidebarItem = ({ icon, text, link }) => {
-        const location = useLocation();
-        const path = location.pathname.slice(1);
+        const SidebarItem = ({ icon, text, link }) => {
+            const location = useLocation();
+            const path = location.pathname.slice(1);
+            return (
+                <div>
+                    <Link
+                        to={link}
+                        className={
+                            path === text.toLowerCase() ||
+                            (text === 'Home' && path === '')
+                                ? 'sidebar-item active'
+                                : 'sidebar-item'
+                        }
+                        title={text}
+                    >
+                        <img src={icon} alt={text + ' icon'} />
+                        <div className="text">{text}</div>
+                    </Link>
+                </div>
+            );
+        };
+
         return (
-            <div>
-                <Link
-                    to={link}
+            <div className={sidebarOpen ? 'sidebar open' : 'sidebar'}>
+                <div className="logo">
+                    <img src={icon_logo} alt="Logo" />
+                    <div className="logo-text">Eindproef</div>
+                </div>
+                <hr />
+                <div className="items main">
+                    <SidebarItem
+                        icon={icon_home}
+                        text="Home"
+                        link={link_home}
+                    />
+                    <SidebarItem
+                        icon={icon_rooms}
+                        text="Rooms"
+                        link={link_rooms}
+                    />
+                    <SidebarItem
+                        icon={icon_discover}
+                        text="Discover"
+                        link={link_discover}
+                    />
+                </div>
+                <hr />
+                <div className="items">
+                    <SidebarItem
+                        icon={icon_components}
+                        text="Components"
+                        link={link_components}
+                    />
+                    <SidebarItem
+                        icon={icon_settings}
+                        text="Settings"
+                        link={link_settings}
+                    />
+                </div>
+                <div
                     className={
-                        path === text.toLowerCase() ||
-                        (text === 'Home' && path === '')
-                            ? 'sidebar-item active'
-                            : 'sidebar-item'
+                        sidebarOpen ? 'width-toggle open' : 'width-toggle'
                     }
-                    title={text}
+                    onClick={toggleSidebar}
                 >
-                    <img src={icon} alt={text + ' icon'} />
-                    <div className='text'>{text}</div>
-                </Link>
+                    <img src={toggleIcon} alt="Toggle" />
+                </div>
             </div>
         );
-    };
-
-    return (
-        <div className={sidebarOpen ? 'sidebar open' : 'sidebar'}>
-            <div className="logo">
-                <img src={icon_logo} alt="Logo" />
-                <div className='logo-text'>Eindproef</div>
-            </div>
-            <hr />
-            <div className="items main">
-                <SidebarItem icon={icon_home} text="Home" link={link_home} />
-                <SidebarItem icon={icon_rooms} text="Rooms" link={link_rooms} />
-                <SidebarItem
-                    icon={icon_discover}
-                    text="Discover"
-                    link={link_discover}
-                />
-            </div>
-            <hr />
-            <div className="items">
-                <SidebarItem
-                    icon={icon_components}
-                    text="Components"
-                    link={link_components}
-                />
-                <SidebarItem
-                    icon={icon_settings}
-                    text="Settings"
-                    link={link_settings}
-                />
-            </div>
-            <div
-                className={sidebarOpen ? 'width-toggle open' : 'width-toggle'}
-                onClick={toggleSidebar}
-            >
-                <img src={toggleIcon} alt="Toggle" />
-            </div>
-        </div>
-    );
-};
+    },
+    (prevProps, nextProps) => {
+        return true;
+    }
+);
 
 export default Sidebar;
